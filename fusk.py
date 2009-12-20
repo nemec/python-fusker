84e90416452fd3c91fdf114ec5436e83cf42cd8e
Dan Nemec <djnemec@gmail.com>
2009-12-19 21:51:54 -0600

    combined both files into one

--- fusk.py
+++ fusk.py
@@ -1,7 +1,24 @@
 #!/bin/python
 
+# This program can take a url in the form www.example.com/photo/pic[0-9].jpg
+# and expand it to 10 different urls or it will read a file containing a list
+# of urls then it will download the data at those urls into a numerically
+# sequential folder.
+# Notes:
+#   Only one set of [] are possible in this version, and there is no error
+#     checking to make sure there is a matching bracket.
+#   Ranges can only be numbers, no letters in this version.
+#   A range from [0-10] translates to 00, 01, 02, etc.
+
+# If true, uses urllib instead of Linux's native wget
+# I should probably get rid of wget and make it always independent,
+# but wget is just so simple!
+independent = True
+
 import sys
 import os
+if independent:
+  import urllib
 
 usage = "Usage: fusk -u url | -f file"
 
@@ -21,13 +38,25 @@ if sys.argv[1] == "-u":
   snd = url[url.find('-')+1:url.find(']')]
   try:
     for i in range(int(fst), int(snd)+1):
-      os.system("wget -P"+str(d)+" "+beg+str(i).zfill(len(snd))+end)
+      # Don't pad with zeroes
+      # pic = str(i)
+      pic = str(i).zfill(len(snd))
+      if independent:
+        urllib.urlretrieve(beg+pic, str(d)+"/"+pic+end)
+      else:
+        os.system("wget -P"+str(d)+" "+beg+pic+end)
   except:
-    os.system("wget -P"+str(d)+" "+url)
+    if independent:
+      urllib.retrieveurl(url, str(d)+"/"+url[url.rfind("/"):])
+    else:
+      os.system("wget -P"+str(d)+" "+url)
 elif sys.argv[1] == "-f":
   with open(url, 'r') as f:
     for line in f:
-      os.system("wget -P"+str(d)+" "+line)
+      if independent:
+        urllib.retrieveurl(line, str(d)+"/"+line[line.rfind("/"):])
+      else:
+        os.system("wget -P"+str(d)+" "+line)
   f.closed
 else:
   print usage
