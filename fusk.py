62534b1e66ab451abd34fedbd447672f297c006b
Dan N <djnemec@gmail.com>
2011-03-20 20:53:41 -0500

    Updated to latest working code

--- fusk.py
+++ fusk.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 
 # This program can take a url in the form www.example.com/photo/pic[0-9].jpg
 # and expand it to 10 different urls or it will read a file containing a list
@@ -13,7 +13,7 @@
 # If true, uses urllib instead of Linux's native wget
 # I should probably get rid of wget and make it always independent,
 # but wget is just so simple!
-independent = True
+independent = False
 
 import sys
 import os
@@ -32,29 +32,33 @@ while os.path.exists(str(d)):
 os.mkdir("./" + str(d) + "/")
 
 if sys.argv[1] == "-u":
-  beg = url[0:url.find('[')]
-  end = url[url.find(']')+1:]
-  fst = url[url.find('[')+1:url.find('-')]
-  snd = url[url.find('-')+1:url.find(']')]
+  ssub = url.find('[')
+  msub = url.find('-',ssub)
+  esub = url.find(']',msub)
+  beg = url[0:ssub]
+  end = url[esub+1:]
+  fst = url[ssub+1:msub]
+  snd = url[msub+1:esub]
   try:
     for i in range(int(fst), int(snd)+1):
       # Don't pad with zeroes
       # pic = str(i)
       pic = str(i).zfill(len(snd))
       if independent:
+        print(str(d)+"/"+pic+end)
         urllib.urlretrieve(beg+pic, str(d)+"/"+pic+end)
       else:
         os.system("wget -P"+str(d)+" "+beg+pic+end)
   except:
     if independent:
-      urllib.retrieveurl(url, str(d)+"/"+url[url.rfind("/"):])
+      urllib.urlretrieve(url, str(d)+"/"+url[url.rfind("/"):])
     else:
       os.system("wget -P"+str(d)+" "+url)
 elif sys.argv[1] == "-f":
   with open(url, 'r') as f:
     for line in f:
       if independent:
-        urllib.retrieveurl(line, str(d)+"/"+line[line.rfind("/"):])
+        urllib.urlretrieve(line, str(d)+"/"+line[line.rfind("/"):])
       else:
         os.system("wget -P"+str(d)+" "+line)
   f.closed
