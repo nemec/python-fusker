eb81c5b25c49ec83be297c72570f3935dec30a9b
Nemec <djnemec@gmail.com>
2011-06-15 23:12:56 -0500

    Restructured, added threading and multiple expansion

--- fusk.py
+++ fusk.py
@@ -10,57 +10,102 @@
 #   Ranges can only be numbers, no letters in this version.
 #   A range from [0-10] translates to 00, 01, 02, etc.
 
-# If true, uses urllib instead of Linux's native wget
-# I should probably get rid of wget and make it always independent,
-# but wget is just so simple!
-independent = False
-
 import sys
 import os
-if independent:
-  import urllib
+import urllib2
+import Queue
+import argparse
+import threading
+  
+parser = argparse.ArgumentParser(description = "Takes a url of the form "
+                      "www.example.com/photos/pic[0-9].jpg and expands it.")
+parser.add_argument("-p", "--pad", default="", type = lambda x: x[0:1],
+                      help = "Single character to pad "
+                      "short expansions with. [0-10] becomes 00, 01, etc. "
+                      "with a padding of \"0\". Defaults to no padding.")
+parser.add_argument("-d", "--dest", default=".", help="Destination to place the"
+                                                " folder containing the files.")
+parser.add_argument("-t", "--threads", default = 1, type = int,
+                    help = "Number of threads.")
+parser.add_argument("-q", "--quiet", action = "store_true")
+group = parser.add_mutually_exclusive_group(required = True)
+group.add_argument("-u", "--url", help = "URL to expand")
+group.add_argument("-f", "--file", help = "File containing a list of URLs to "
+                                          "download.")
+                                          
+args = parser.parse_args()
 
-usage = "Usage: fusk -u url | -f file"
-
-if len(sys.argv) != 3:
-  print usage
-
-url=sys.argv[2];
-d=0
-while os.path.exists(str(d)):
-  d=d+1
-os.mkdir("./" + str(d) + "/")
-
-if sys.argv[1] == "-u":
-  ssub = url.find('[')
-  msub = url.find('-',ssub)
-  esub = url.find(']',msub)
-  beg = url[0:ssub]
-  end = url[esub+1:]
-  fst = url[ssub+1:msub]
-  snd = url[msub+1:esub]
+def expand(url):
+  parts = []
   try:
+    ssub = url.find('[')
+    msub = url.find('-',ssub)
+    esub = url.find(']',msub)
+    beg = url[0:ssub]
+    end = url[esub+1:]
+    fst = url[ssub+1:msub]
+    snd = url[msub+1:esub]
     for i in range(int(fst), int(snd)+1):
-      # Don't pad with zeroes
-      # pic = str(i)
-      pic = str(i).zfill(len(snd))
-      if independent:
-        print(str(d)+"/"+pic+end)
-        urllib.urlretrieve(beg+pic, str(d)+"/"+pic+end)
+      if args.pad:
+        pic = str(i).rjust(len(snd), args.pad)
       else:
-        os.system("wget -P"+str(d)+" "+beg+pic+end)
-  except:
-    if independent:
-      urllib.urlretrieve(url, str(d)+"/"+url[url.rfind("/"):])
-    else:
-      os.system("wget -P"+str(d)+" "+url)
-elif sys.argv[1] == "-f":
-  with open(url, 'r') as f:
+        pic = str(i)
+      parts.append(beg+pic)
+  except ValueError:
+    pass
+    
+  files = []
+  for f in parts:
+    for rest in expand(end):
+      files.append(f + rest)
+      
+  return files or [url]
+
+def get_next_dir(dest):
+  d=0
+  while os.path.exists(str(d)):
+    d=d+1
+  return str(d)
+destination_dir = os.path.join(args.dest, get_next_dir(args.dest))
+os.mkdir(destination_dir)
+
+def thread_work(files, dest_dir):
+  while True:
+    url = files.get()
+    try:
+      if not args.quiet:
+        print "Downloading {} to {}".format(url, dest_dir)
+      try:
+        filepath = os.path.join(destination_dir, url[url.rfind(os.sep):])
+        filename = filepath[filepath.rfind(os.sep):]
+        request = urllib2.urlopen(url)
+        f = open(os.path.join(dest_dir) + filename, 'wb')
+        f.write(request.read())
+        f.close()
+      except urllib2.HTTPError as e:
+        if e.code == 404:
+          print "404: {} does not exist on the server.".format(url)
+    finally:
+      files.task_done()
+  return
+      
+
+files = Queue.Queue()
+
+if args.url:
+  for url in expand(args.url):
+    files.put(url)
+elif args.file:
+  with open(args.file, 'r') as f:
     for line in f:
-      if independent:
-        urllib.urlretrieve(line, str(d)+"/"+line[line.rfind("/"):])
-      else:
-        os.system("wget -P"+str(d)+" "+line)
-  f.closed
-else:
-  print usage
+      files.append(line)
+  f.close()
+  
+threads = []
+for x in range(min(args.threads, files.qsize())):
+  newthread = threading.Thread(target = thread_work, args = (files, destination_dir))
+  newthread.daemon = True
+  threads.append(newthread)
+  newthread.start()
+
+files.join()
